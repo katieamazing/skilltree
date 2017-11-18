a0f9457368bc4080c1a68705e64a61fa405393e2
katieamazing <k.allen@live.com>
2017-11-18 16:02:30 -0500

    Starting to add images

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 var app = new Vue({
   el: "#app",
   data: {
+    isIconPanelOpen: false,
     skills: [
-      { text: "Scratch ur bum" }
+      { img: "icon", text: "Scratch ur bum" }
+    ],
+    edges: [
     ],
     new_skill: "Add a new skill",
-    current_source: null
+    current_source: null,
+    current_icon: null
   },
   methods: {
+    openIconPanel: function () {
+      this.isIconPanelOpen = true;
+    },
+    selectSkillIcon: function (icon_id) {
+      this.current_icon = icon_id;
+      this.isIconPanelOpen = false;
+    },
     addSkill: function () {
-      this.skills.push({text: this.new_skill})
+      this.skills.push({img: this.current_icon, text: this.new_skill})
     },
     focusSkill: function (skill_id) {
       if (this.current_source === null) {
         this.current_source = skill_id;
       } else {
         console.log("creating an edge between " + this.current_source + ", " + skill_id);
+        this.edges.push({ src: this.current_source, dst: skill_id });
         this.current_source = null;
       }
     },
