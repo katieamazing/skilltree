a4af3e4480f2051c1c78315ad8a61871baf3db86
katieamazing <k.allen@live.com>
2017-11-28 16:53:48 -0500

    Some new traversal stuff

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ var app = new Vue({
       { alt: "swords", src: "imgs/dummyimg.jpg" }
     ],
     skills: [
-      { img: 0, text: "Be a man", unlocks: [1], x: 100, y: 100 },
-      { img: 0, text: "Swift as a coursing river", unlocks: [2], x: 100, y: 200 },
-      { img: 0, text: "Force of a great typhoon", unlocks: [], x: 100, y: 300 }
+      // { img: 0, text: "Be a man", unlocks: [1], x: 100, y: 100 },
+      // { img: 0, text: "Swift as a coursing river", unlocks: [2], x: 100, y: 200 },
+      // { img: 0, text: "Force of a great typhoon", unlocks: [], x: 100, y: 300 }
     ],
     new_skill: "Add a new skill",
-    edges: [ {src: 0, dst: 1}, {src: 1, dst: 2} ],
+    edges: [
+//      {src: 0, dst: 1}, {src: 1, dst: 2}
+    ],
     current_source: null,
     current_icon: null,
     width: 800,
@@ -38,29 +40,64 @@ var app = new Vue({
         this.skills[this.current_source].unlocks.push(skill_id);
         this.edges.push({src: this.current_source, dst: skill_id});
         this.current_source = null;
-
+        this.traverse();
       }
     },
     clearSelection: function () {
       this.current_source = null;
     },
+    traverseToFindWidest: function () {
+      var seen = [];
+      for (var i = 0; i < this.skills.length; i++) {
+        this.traverseToFindWidestStartingAt(i, seen, 1);
+      }
+      console.log(seen);
+      var histogram = [];
+      for (var j = 0; j < seen.length; j++) {
+        var depth = seen[j];
+        if (histogram[depth] !== undefined) {
+          histogram[depth] = histogram[depth] + 1;
+        } else {
+          histogram[depth] = 1;
+        }
+      }
+      console.log(histogram);
+      var widest = 0;
+      for (var k = 1; k < histogram.length; k++) {
+          widest = Math.max(widest, histogram[k]);
+      }
+      return widest;
+    },
+    traverseToFindWidestStartingAt: function (i, seen, depth) {
+      if (seen[i]) {
+        return
+      } else {
+        seen[i] = depth;
+        for (var j = 0; j < this.skills[i].unlocks.length; j++) {
+          this.traverseToFindWidestStartingAt(this.skills[i].unlocks[j], seen, depth + 1);
+        }
+      }
+    },
     traverse: function () {
       let seen = [];
       for (var i = 0; i < this.skills.length; i++) {
-        this.traverseStartingAt(i, seen);
+        this.traverseStartingAt(i, seen, 1);
       }
+      let width = this.traverseToFindWidest();
+      console.log("width is " + width);
     },
-    traverseStartingAt: function (i, seen) {
+    traverseStartingAt: function (i, seen, depth) {
       if (seen[i]) {
         console.log("Base case");
         console.log(seen);
         return
       } else {
         seen[i] = true;
+        this.skills[i].y = depth * 100;
         console.log("seen " + i);
         console.log(seen);
         for (var j = 0; j < this.skills[i].unlocks.length; j++) {
-          this.traverseStartingAt(j, seen);
+          this.traverseStartingAt(this.skills[i].unlocks[j], seen, depth + 1);
         }
       }
     }
